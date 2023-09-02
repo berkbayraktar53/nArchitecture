a4fdd992074d6ca8ac0e72e84f1bbd8fc8bf0747
Kerem Berk Bayraktar <keremberk53@gmail.com>
2023-09-02 21:21:25 +0300

    CQRS and Mediatr implemented

---
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+        {
+            public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse
+                {
+                    Id = new Guid(),
+                    Name = request.Name
+                };
+                return null;
+            }
+        }
+    }
+}
---
+++ src/rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Features.Brands.Commands.Create;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
